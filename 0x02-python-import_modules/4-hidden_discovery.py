c5ceb99375f3b97ee35a0f226415b246700bb4d1
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-06-02 03:34:41 -0700

    python modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     content = dir(hidden_4)
     for name in content:
         if name[:2] != "__":
-            print("{}".format(name)
+            print("{}".format(name))
