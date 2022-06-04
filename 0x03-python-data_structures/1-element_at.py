fde6d2f0c3371ace557948a76519c45620ac63a7
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-06-04 05:02:24 -0700

    python

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        print("None")
+    elif idx > len(my_list):
+        print("None") 
+    else:
+        print("{:d}".format(my_list[]))
