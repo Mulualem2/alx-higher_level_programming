f042dffd204dd9582f70ddef06c2fa0e31b73d71
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-07-04 11:48:02 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function to create basegeometry
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """Function not implemented"""
+        raise Exception("area() is not implemented")
