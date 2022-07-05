e36ae60e44d8b192f67e7805a5ab1d975ef4e6df
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-07-05 06:34:02 -0700

    input out put in python

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3ads from standard input and computes metrics.
+#!/usr/bin/python3
 
-"""
+"""Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
