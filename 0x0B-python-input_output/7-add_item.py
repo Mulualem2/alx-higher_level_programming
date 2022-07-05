17a194c7d732eddb1159af32a7ed3e8c4868e6e8
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-07-05 06:19:11 -0700

    input out put in python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """add an item"""
-from sys import argv
-import json
-import os
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-
-if os.path.isfile("add_item.json"):
-    my_list = load_from_json_file("add_item.json")
-else:
-    my_list = []
-
-for arg in range(1, len(argv)):
-    my_list.append(argv[arg])
-save_to_json_file(my_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
