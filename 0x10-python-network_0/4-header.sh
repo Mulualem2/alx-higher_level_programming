8568730a6976e19907a9200848c07bb0a84b97d3
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-09-22 03:02:10 -0700

    updating task 7

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes a URL
-curl -sH "X-School-User-Id: 98" "$1"
+curl -s X GET -H "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,19 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    """find a peak in a list of unsorted integers"""
-    if len(list_of_integers) is 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    elif len(list_of_integers) == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    elif len(list_of_integers) == 2:
+    elif size == 2:
         return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        mid_idx = int(len(list_of_integers) / 2)
-        if list_of_integers[mid_idx] > list_of_integers[mid_idx + 1]:
-            if list_of_integers[mid_idx] > list_of_integers[mid_idx - 1]:
-                return list_of_integers[mid_idx]
-            else:
-                return find_peak(list_of_integers[:mid_idx])
-        else:
-            return find_peak(list_of_integers[mid_idx + 1:])
+        return find_peak(list_of_integers[mid + 1:])
