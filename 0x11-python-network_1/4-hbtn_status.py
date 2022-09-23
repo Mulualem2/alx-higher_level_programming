f46d84544da12b202860d78c15bc51467b89f8fc
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-09-23 03:24:23 -0700

    updating task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,9 @@ Script that fetches https://intranet.hbtn.io/status
 '''
 
 import requests
+
 if __name__ == "__main__":
-    request = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(request.text), request.text))
+    url = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
