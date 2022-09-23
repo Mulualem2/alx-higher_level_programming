25506f691ef4ecff598892e6ede21089cc86403f
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-09-23 02:43:16 -0700

    fixing bugs task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,14 @@
 '''
 Script that fetches https://intranet.hbtn.io/status
 '''
-from urllib import request
 
-url = 'https://intranet.hbtn.io/status'
-with request.urlopen(url) as response:
-    the_page = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(the_page)))
-    print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+if __name__ == '__main__':
+    from urllib import request
+
+    url = 'https://intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        the_page = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
